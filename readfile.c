e854d707116b7114520a26417d2615912dec25ec
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-08-27 18:50:18 +0530

    Tokenizing the string read from the disk

--- readfile.c
+++ readfile.c
@@ -3,23 +3,39 @@
 #include <stdlib.h>
 #include <time.h>
 #include "structure.c"
-/*typedef struct Row{
-	int id; // The Number of datapoint.
-	double crim; // Per Capita Crime Rate by town.
-	double zn;  // proportion of residential land zoned for lots over 25,000 sq.ft.
-	double indus; // proportion of non-retail business acres per town.
-	double chas; // Charles River dummy variable (= 1 if tract bounds river; 0 otherwise).
-	double nox; // nitrogen oxides concentration (parts per 10 million).
-	double rm; // average number of rooms per dwelling.
-	double age; // proportion of owner-occupied units built prior to 1940.
-	double dis; // weighted mean of distances to five Boston employment centres.
-	int rad; // index of accessibility to radial highways.
-	int tax; // full-value property-tax rate per $10,000.
-	double ptratio; // pupil-teacher ratio by town.
-	double black; // 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.
-	double lstat; // lower status of the population (percent).
-	double medv; // median value of owner-occupied homes in $1000s.
-}row;*/ 
+ 
+// id,date,price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,condition,grade,sqft_above,sqft_basement,yr_built,yr_renovated,zipcode,lat,long,sqft_living15,sqft_lot15
+// 7129300520,20141013T000000,221900,3,1,1180,5650,1,0,0,3,7,1180,0,1955,0,98178,47.5112,-122.257,1340,5650
+
+char** split(char *str, char delimiter){
+	char **parts = malloc(30 * sizeof(char *));
+	char temp[30][30];
+	char s[25];
+	int i = 0, k = 0, j = 0;
+	for(i = 0 ; i < strlen(str) + 1 ; i++)
+		if(str[i] == ',' || str[i] == '\0'){
+			s[j++] = '\0';
+			strcpy(temp[k], s);
+			k = k + 1;
+			s[0] = '\0';
+			j = 0;
+		}else{
+			s[j++] = str[i];
+		}
+		
+	for(i = 0 ; i < 25 ; i++){
+		parts[i] = malloc(30 * sizeof(char));
+		strcpy(parts[i], temp[i]);
+	}
+	return parts;
+}
+
+row parseString(char *str){
+	char **token = split(str, ',');
+	int i = 0;
+	for(i =0  ;i  < 21 ; i++)
+		printf("%s\n", token[i]);
+}
 
 char** read_csv(char *str){
 	// Open the file on disk.
@@ -36,7 +52,7 @@ char** read_csv(char *str){
 	char **data = malloc(21615 * sizeof(char *));
 	int j;
 	for(j = 0 ; j < 21615 ; j++)
-		data[j] = malloc(1000 * sizeof(char));
+		data[j] = malloc(500 * sizeof(char));
 	j = 0;
 	do{
 		fscanf(fp, "%s", data[j]);
@@ -56,5 +72,7 @@ int main(){
 	int i;
 	for(i =0  ; i < 10 ; i++)
 		printf("%s\n", data[i]);
+		
+	row r = parseString("7129300520,20141013T000000,221900,3,1,1180,5650,1,0,0,3,7,1180,0,1955,0,98178,47.5112,-122.257,1340,5650");
 	return 0;
 }
