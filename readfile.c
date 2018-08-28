e1c84be226bff2cb88121ceec8bfa245bafa541b
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-08-28 21:28:10 +0530

    Reading CSV into Structure Done!

--- readfile.c
+++ readfile.c
@@ -1,14 +1,15 @@
+// Maintain this header file order..... Very Important.
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include "structure.c"
 #include "helper.c"
- 
 // id,date,price,bedrooms,bathrooms,sqft_living,sqft_lot,floors,waterfront,view,condition,grade,sqft_above,sqft_basement,yr_built,yr_renovated,zipcode,lat,long,sqft_living15,sqft_lot15
 // 7129300520,20141013T000000,221900,3,1,1180,5650,1,0,0,3,7,1180,0,1955,0,98178,47.5112,-122.257,1340,5650
 
-char** read_csv(char *str){
+// This function takes the name of file on disk as an argument and returns the dataset in structure format.
+row* read_csv(char *str){
 	// Open the file on disk.
 	FILE *fp;
 	fp = fopen(str, "r");
@@ -30,20 +31,25 @@ char** read_csv(char *str){
 		j++;
 	}while(!feof(fp));
 	
-	clock_t end = clock();
+	row *r = malloc(21610 * sizeof(row));
+	int i;
+	for(i = 0 ; i < 21610 ; i++){	
+		r[i] = parseString(data[i+1]);
+		free(data[i+1]);
+	}
 	
+	clock_t end = clock();
 	timespent = (end-start)/(CLOCKS_PER_SEC/1000);
+	
+	
 	printf("\nLoading - Done\nTime Taken to load the dataset : %fms\n", timespent);
-	printf("_________________________________________________________\n");
-	return data;
+	printf("_________________________________________________________\n\n");
+	
+	return r;
 }
 
 int main(){
-	char **data = read_csv("kc_house_data.csv");
-	int i;
-	for(i =0  ; i < 10 ; i++)
-		printf("%s\n", data[i]);
-		
-	row r = parseString("7129300520,20141013T000000,221900,3,1,1180,5650,1,0,0,3,7,1180,0,1955,0,98178,47.5112,-122.257,1340,5650");
+	row *r = read_csv("kc_house_data.csv");
+	printDataset(r, 100);
 	return 0;
 }
